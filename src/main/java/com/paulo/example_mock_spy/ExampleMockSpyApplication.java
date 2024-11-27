30657036488580166dcb050ed74fea10ec5d867c
phpontes <ppontes95@hotmail.com>
2024-11-27 14:31:33 -0300

    Project Created

---
+++ src/main/java/com/paulo/example_mock_spy/ExampleMockSpyApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.example_mock_spy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMockSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMockSpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/example_mock_spy/ExampleMockSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.example_mock_spy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMockSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
